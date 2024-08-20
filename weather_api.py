39595889a77ecf859bc034de3205ef5fd1ecd14d
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-20 21:16:30 +0530

    2 new featured added i.e. if u type wrong location name it will provie a error message with a back ground image

--- weather_api.py
+++ weather_api.py
@@ -3,12 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-API_KEY = 'Your API Key'
+API_KEY = '85bb01ad3a213287e55a2d7156c2cc7b'
 BASE_URL = 'http://api.openweathermap.org/data/2.5/weather'
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     weather = None
+    var = False
     background_image = None
     if request.method == 'POST':
         city = request.form.get('city')
@@ -18,23 +19,34 @@ def index():
                 'appid': API_KEY,
                 'units': 'metric'
             })
+            print(response.status_code)
+            #if response.status_code != 200:
+
             if response.status_code == 200:
-                weather = response.json()
-                weather_condition = weather['weather'][0]['main'].lower()
+               weather = response.json()
+               print(weather)
+               var = True
+                
+                
+               weather_condition = weather['weather'][0]['main'].lower()
                 # Map weather conditions to background images
-                background_map = {
-                    'clear': 'sunny.jpg',
-                    'clouds': 'cloudy.jpg',
-                    'rain': 'rainy.jpg',
-                    'snow': 'snowy.jpg',
-                    'storm': 'stormy.jpg',
-                    'haze': 'haze.jpg'
-                }
-                background_image = background_map.get(weather_condition, 'default.jpg')  # default image if no match
+               background_map = {
+                        'clear': 'sunny.jpg',
+                        'clouds': 'cloudy.jpg',
+                        'rain': 'rainy.jpg',
+                        'snow': 'snowy.jpg',
+                        'storm': 'stormy.jpg',
+                        'haze': 'haze.jpg',
+                        'mist':'mist.jpg'
+                    }
+               background_image = background_map.get(weather_condition, 'default.jpg')  # default image if no match
             else:
                 weather = {'message': 'City not found or API error.'}
+                background_image = 'thank_you.jpg'
+                
+            
     
-    return render_template('index.html', weather=weather, background_image=background_image)
+    return render_template('index.html',var = var, weather=weather, background_image=background_image)
 
 if __name__ == '__main__':
     app.run(debug=True)
